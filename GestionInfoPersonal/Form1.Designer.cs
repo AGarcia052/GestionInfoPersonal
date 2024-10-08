a07ab51cfa60b248869439c0fae3e3f51e7d427c
AGarcia05 <agarciadelaplazaperulero@gmail.com>
2024-10-08 08:48:23 +0200

    Ajustes formulario principal y creacion de TabControl

--- GestionInfoPersonal/Form1.Designer.cs
+++ GestionInfoPersonal/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tbCPrincipal = new TabControl();
+            tbPDatos = new TabPage();
+            tbPPreferencias = new TabPage();
+            tbPVisualizacion = new TabPage();
+            tbCPrincipal.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tbCPrincipal
+            // 
+            tbCPrincipal.Controls.Add(tbPDatos);
+            tbCPrincipal.Controls.Add(tbPPreferencias);
+            tbCPrincipal.Controls.Add(tbPVisualizacion);
+            tbCPrincipal.Location = new Point(25, 12);
+            tbCPrincipal.Name = "tbCPrincipal";
+            tbCPrincipal.SelectedIndex = 0;
+            tbCPrincipal.Size = new Size(741, 405);
+            tbCPrincipal.TabIndex = 0;
+            // 
+            // tbPDatos
+            // 
+            tbPDatos.Location = new Point(4, 29);
+            tbPDatos.Name = "tbPDatos";
+            tbPDatos.Padding = new Padding(3);
+            tbPDatos.Size = new Size(733, 372);
+            tbPDatos.TabIndex = 0;
+            tbPDatos.Text = "Datos Personales";
+            tbPDatos.UseVisualStyleBackColor = true;
+            // 
+            // tbPPreferencias
+            // 
+            tbPPreferencias.Location = new Point(4, 29);
+            tbPPreferencias.Name = "tbPPreferencias";
+            tbPPreferencias.Padding = new Padding(3);
+            tbPPreferencias.Size = new Size(733, 372);
+            tbPPreferencias.TabIndex = 1;
+            tbPPreferencias.Text = "Preferencias";
+            tbPPreferencias.UseVisualStyleBackColor = true;
+            // 
+            // tbPVisualizacion
+            // 
+            tbPVisualizacion.Location = new Point(4, 29);
+            tbPVisualizacion.Name = "tbPVisualizacion";
+            tbPVisualizacion.Size = new Size(733, 372);
+            tbPVisualizacion.TabIndex = 2;
+            tbPVisualizacion.Text = "Visualización";
+            tbPVisualizacion.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tbCPrincipal);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "FormPrincipal";
+            tbCPrincipal.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tbCPrincipal;
+        private TabPage tbPDatos;
+        private TabPage tbPPreferencias;
+        private TabPage tbPVisualizacion;
     }
 }
