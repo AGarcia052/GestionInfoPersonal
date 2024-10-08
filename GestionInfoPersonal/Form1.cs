1fadad534a8c584ab1e0d088d3f9438a3143f3bf
AGarcia05 <agarciadelaplazaperulero@gmail.com>
2024-10-08 09:22:28 +0200

    Creacion tab datos y funcionalidad numeric up down

--- GestionInfoPersonal/Form1.cs
+++ GestionInfoPersonal/Form1.cs
@@ -6,5 +6,13 @@ namespace GestionInfoPersonal
         {
             InitializeComponent();
         }
+
+
+        private void numUPEdad_ValueChanged(object sender, EventArgs e)
+        {
+            int edad = (int)numUPEdad.Value;
+            mthCFechaNacimiento.SetDate(DateTime.Now.AddYears(-edad));
+
+        }
     }
 }
